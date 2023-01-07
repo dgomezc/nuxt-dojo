4d83992d2834775fb260853f7634bc67cc12f185
David Gómez <v-davgom@microsoft.com>
2023-01-07 19:05:14 +0100

    Adding pages

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad consectetur, eius ratione cumque vitae pariatur
+            recusandae quia delectus ducimus earum expedita debitis autem, maiores molestias inventore alias aperiam
+            blanditiis voluptates?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
