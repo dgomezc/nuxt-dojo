aca350474e7c3d1be5100032505d8f5e2a23bb82
David Gómez <v-davgom@microsoft.com>
2023-01-07 19:14:12 +0100

    Route parameters and NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+                <ul>
+                    <li>
+                <NuxtLink to="/">Home</NuxtLink></li>
+                    <li>
+                <NuxtLink to="/about">About</NuxtLink></li>
+                    <li>
+                <NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
         <h2>Home</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad consectetur, eius ratione cumque vitae pariatur
             recusandae quia delectus ducimus earum expedita debitis autem, maiores molestias inventore alias aperiam
---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veniam
+      cupiditate tempore necessitatibus adipisci iusto voluptatibus, dolorem    
+      consequatur vel, quis fugit officia. Placeat neque aut accusantium non
+      quasi ut cupiditate.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
