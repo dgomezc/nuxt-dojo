df88d55509e47781464469aa634773bff46089dc
David Gómez <v-davgom@microsoft.com>
2023-01-07 19:52:41 +0100

    Add Tailwind CSS

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/products">Nuxt Dojo Merj</NuxtLink>
+      <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merj</NuxtLink>
       </nav>
     </header>
 
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
 
-    <footer>
-      <ul>
+    <footer class="container mx-4 p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li>
           <NuxtLink to="/">Home</NuxtLink>
         </li>
