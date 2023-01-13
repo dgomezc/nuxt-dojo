9aa16a963430a1b5ca8c0d81a05c784ea9fb55fa
David Gómez <v-davgom@microsoft.com>
2023-01-13 18:08:35 +0100

    Dynamic server routes

--- pages/about.vue
+++ pages/about.vue
@@ -8,6 +8,7 @@
       blanditiis voluptates?
     </p>
     <div>{{ data }}</div>
+    <div>{{ currencyData }}</div>
   </div>
 </template>
 
@@ -16,6 +17,7 @@ const { data } = await useFetch("api/ninja?name=mario", {
   method: "post",
   body: { age: 30 },
 });
+const { data: currencyData } = await useFetch("api/currency/GBP");
 </script>
 
 <style scoped>
